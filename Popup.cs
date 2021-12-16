4bb4d881f4509a57545d23ace8d3ee99727a12d3
DanielMoro24 <73697185+DanielMoro24@users.noreply.github.com>
2021-12-16 23:32:05 +0100

    initial commit

---
+++ Popup.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Biblioteca
+{
+    public partial class Popup : Form
+    {
+        public Popup()
+        {
+            InitializeComponent();
+            
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Main main = new Main();
+            main.Visible = true;
+            this.Close();
+        }
+    }
+}
